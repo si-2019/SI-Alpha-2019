15c7af23a37669b43e48471ff03f03c778d0c156
zkaric1 <zkaric1@etf.unsa.ba>
2019-04-14 19:16:23 +0200

    dodan kontroler

--- Admnistracija/AdmnistracijaApi/Controllers/KorisnikController.cs
+++ Admnistracija/AdmnistracijaApi/Controllers/KorisnikController.cs
@@ -4,10 +4,25 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using Administracija.DAL.Interfaces;
 
 namespace AdmnistracijaApi.Controllers
 {
     public class KorisnikController : ApiController
     {
+        private  IKorisnikRepository _korisnikRepository;
+
+       // public KorisnikController() { }
+        public KorisnikController(IKorisnikRepository korisnikRepository) {
+            _korisnikRepository = korisnikRepository;
+        }
+
+        [HttpGet]
+        [HttpOptions]
+        [ActionName("GetLoginDataForProfessor")]
+        public IHttpActionResult GetLoginDataForProfessor(string ime, string prezime) {
+            return Ok(_korisnikRepository.GenerateLoginDataForProfessor(ime, prezime));
+        }
     }
 }
+
