5cbcdb5c108d7f41dea120204384c16601d50778
zkaric1 <zkaric1@etf.unsa.ba>
2019-05-03 16:22:33 +0200

    Dodavanje profesora u bazu

--- Routes/korisnikController.js
+++ Routes/korisnikController.js
@@ -7,8 +7,9 @@ var md5 = require('md5');
 const db = require('../models/db.js');
 const XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest;
 db.sequelize.sync();
-const Op = db.Sequelize.Op;
+//var validacija = require('./validacija.js');
 
+//generisanje username-a i passworda za profesora
 korisnikRouter.get('/GetLoginDataForProfessor',function(req,res){
     var ime = req.query.ime;
     var prezime = req.query.prezime;
@@ -16,16 +17,8 @@ korisnikRouter.get('/GetLoginDataForProfessor',function(req,res){
     var userName = ime + '.'+ prezime;
     userName = userName.toLowerCase();
     console.log(userName);
-    db.Korisnik.findAll({
-        where:{
-            username: {
-                [Op.like]: `${userName}%`
-            }
-        },
-        attributes:['username']
-        
-    })
-    .then(data => {
+
+    db.Korisnik.findAll({where:{ime:ime,prezime:prezime}}).then(data => {
         var brojDuplikata = data.length;
         console.log(brojDuplikata);
         userName += ++brojDuplikata;
@@ -44,4 +37,46 @@ korisnikRouter.get('/GetLoginDataForProfessor',function(req,res){
         res.end(err);
     });
 })
+
+//dodavanje profesora u bazu
+korisnikRouter.post('/AddNewProfessor',function(req,res) {
+    let body = req.body;
+    if(!body.spol || !body.ime || !body.prezime) {
+        res.status(400).end("Neispravan foramt");
+    }
+
+    db.Odsjek.findOne({where:{naziv:body.idOdsjek}}).then(odsjek =>{
+        body.idOdsjek = odsjek.idOdsjek;
+        console.log('Odjsek'+odsjek.idOdsjek);
+    })
+    var parts = body.datumRodjenja.split('-');
+    var date = new Date(parts[0], parts[1] - 1, parts[2]); 
+    
+    body.datumRodjenja = date;
+    console.log(body);
+    var ajax = new XMLHttpRequest();
+    ajax.onreadystatechange = function() {
+        if(ajax.readyState == 4 && ajax.status == 200) {
+            var data = JSON.parse(ajax.responseText);
+            console.log(data);
+            if(body.spol.toLowerCase() == 'musko' || body.spol.toLowerCase() == 'muško')
+                body.spol = 1;            
+            else if(body.spol.toLowerCase() == 'zensko' || body.spol.toLowerCase() == 'žensko')
+                body.spol = 0;
+
+            body.JMBG = body.jmbg;
+            body.idUloga = 3;
+            body.username = data.username;
+            body.password = md5(data.password);
+            body.indeks = null;
+            db.Korisnik.create(body);   
+            console.log("tu");   
+            res.end("Uspješno dodan korisnik " + body.username);
+          
+        }
+    }
+    ajax.open('GET', 'http://localhost:31901/api/korisnik/GetLoginDataForProfessor?ime=' + body.ime + '&prezime=' + body.prezime, true);
+    ajax.setRequestHeader('Content-Type','application/json');
+    ajax.send();  
+});
 module.exports = korisnikRouter;
