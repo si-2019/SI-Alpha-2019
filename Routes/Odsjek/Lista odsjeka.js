fd79452115ab575f0751a964263f27322f74d5d5
ATalic2 <you@example.com>
2019-05-04 20:12:57 +0200

    Lista odjseka

---
+++ Routes/Odsjek/Lista odsjeka.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const odsjekRouter = express.Router();
+
+const db = require('../../models/db.js');
+db.sequelize.sync();
+
+odsjekRouter.get('/GetOdsjeci', function(req, res){
+	console.log("Pretražuje bazu podataka");
+	db.Odsjek.findAll({
+    })
+    .then(data => {
+		//data sadrzi listu odsjeka, pretvara se u JSON i šalje
+		console.log("Vraća se lista odsjeka\n");
+		res.status(200).end(JSON.stringify(data));
+	});
+});
+
+module.exports = odsjekRouter;
