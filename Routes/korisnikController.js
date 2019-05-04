e0b22793725f024b18771fd89588043db82339b4
zkaric1 <zkaric1@etf.unsa.ba>
2019-05-05 00:20:25 +0200

    dodani testovi

--- Routes/korisnikController.js
+++ Routes/korisnikController.js
@@ -7,35 +7,27 @@ var md5 = require('md5');
 const db = require('../models/db.js');
 const XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest;
 db.sequelize.sync();
-require('./validacija.js')();
 
 
 //brisanje predmeta po nazivu
 //link : http://localhost:31901/api/korisnik/deleteSubject?naziv=test Predmet za brisanje 4
 korisnikRouter.delete('/deleteSubject', function(req,res) {
-    console.log(req.query.naziv);
     db.Predmet.findAll({where: {naziv: req.query.naziv}}).then( predmet => {
-        console.log(predmet);
         res.contentType('application/json');
         if(predmet != []) {
         db.Predmet.destroy({where: {naziv: req.query.naziv}}).then( function(rowDeleted){
             if(rowDeleted == 1) {
-                console.log('Uspjesno obrisano');
                 res.status(200).send({message: 'Uspjesno obrisan predmet'})
             }
-            else {
-            
+            else {            
             res.status(400).send({message: 'Ne postoji predmet sa tim nazivom'})
             }
         }, function(err) {
             console.log(err);
-        })
-        } 
-        else {
-            res.status(400).send('Ne postoji predmet sa tim nazivom')
-        }
-    })
+        })   
+    }
    
+})
 })
 
 module.exports = korisnikRouter;
