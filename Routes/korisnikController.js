ab331ee0df66a4df3c8153eb7098337f052de9e9
zkaric1 <zkaric1@etf.unsa.ba>
2019-05-04 18:51:52 +0200

    dodana pretraga

--- Routes/korisnikController.js
+++ Routes/korisnikController.js
@@ -8,5 +8,71 @@ const db = require('../models/db.js');
 const XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest;
 db.sequelize.sync();
 
+global.JsonNiz = async function(korisnici,res) {
+  
+    var niz = [];
+    for(var el in korisnici) {
+       await db.Odsjek.findOne({where: {idOdsjek: korisnici[el].idOdsjek}}).then( odsjek => {
+            var json = {
+                odsjek: odsjek.naziv, 
+                ime: korisnici[el].ime, 
+                prezime: korisnici[el].prezime, 
+                datumRodjenja: korisnici[el].datumRodjenja, 
+                JMBG: korisnici[el].JMBG, 
+                email: korisnici[el].email,
+                mjestoRodjenja: korisnici[el].mjestoRodjenja,
+                kanton: korisnici[el].kanton,
+                drzavljanstvo: korisnici[el].drzavljanstvo,
+                telefon: korisnici[el].telefon,
+                spol: korisnici[el].spol,  //musko zensko
+                imePrezimeMajke: korisnici[el].imePrezimeMajke,
+                imePrezimeOca: korisnici[el].imePrezimeOca,
+                adresa: korisnici[el].adresa,
+                username: korisnici[el].username,
+                //password:null
+                linkedin: korisnici[el].linkedin,
+                website: korisnici[el].website,
+                titula: korisnici[el].titula};
+            niz.push(json);
+        })
+       
+       }      
+      
+       if(niz.length != 0) {
+         res.write(JSON.stringify(niz));  
+         return res.end();
+       }
+       else {
+        return res.status(400).send({message: 'Ne postoji u sistemu'});
+        }
+}
+
+//pretraga profesora 
+korisnikRouter.get('/searchProfessor', function(req,res){
+    console.log(req.query);
+    res.contentType('application/json');
+    if(req.query.ime != null && req.query.ime != '' && req.query.prezime != null && req.query.prezime != '') { // ime i prezime
+        db.Korisnik.findAll({where: {idUloga: 3, ime: req.query.ime, prezime: req.query.prezime}}).then( korisnici => {
+            JsonNiz(korisnici,res);            
+        })
+    }
+    else if(req.query.ime != null && req.query.ime != '') { //ime
+       db.Korisnik.findAll({where: {idUloga: 3, ime: req.query.ime}}).then( korisnici => {
+           JsonNiz(korisnici,res);
+       })
+    }
+    else if(req.query.prezime != null && req.query.prezime != '') { //prezime
+        db.Korisnik.findAll({where: {idUloga: 3, prezime: req.query.prezime}}).then( korisnici => {
+            JsonNiz(korisnici,res);            
+        })
+    }
+    else if(req.query.username != null && req.query.username != '') { //username
+        db.Korisnik.findAll({where: {idUloga: 3, username: req.query.username}}).then( korisnici => {
+            JsonNiz(korisnici,res);      
+           
+        })
+    }
+})
+
 
 module.exports = korisnikRouter;
