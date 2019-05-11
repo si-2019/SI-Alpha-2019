7fbe220fb5f201a06c72f2ba046124ea24d87be6
ATalic2 <you@example.com>
2019-05-11 11:33:20 +0200

    Administrator povezuje predmet-odsjek-semestar-ciklus-tip i stavlja vezu u bazi podataka

--- models/Odsjek.js
+++ models/Odsjek.js
@@ -1,18 +1,18 @@
-/* jshint indent: 2 */
+const Sequelize = require("sequelize");
 
-module.exports = function(sequelize, DataTypes) {
-    return sequelize.define('Odsjek', {
-      idOdsjek: {
-        type: DataTypes.INTEGER(10),
-        allowNull: false,
-        primaryKey: true
-      },
-      naziv: {
-        type: DataTypes.STRING(25),
-        allowNull: true
-      }
-    }, {
-      tableName: 'Odsjek'
-    });
-  };
-
+module.exports = function(sequelize,DataTypes){
+    return sequelize.define("Odsjek",{
+		idOdsjek: {
+			type: DataTypes.INTEGER(10),
+			allowNull: false,
+			autoIncrement: true,
+			primaryKey: true
+		},
+        naziv:{
+			type: Sequelize.STRING(25),
+			allowNull: true
+		}
+    },{
+		tableName: 'Odsjek'
+	})
+};
