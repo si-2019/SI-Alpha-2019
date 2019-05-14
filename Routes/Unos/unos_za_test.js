c1c91b502f2e2b868d86a0b122d26d76276cd81e
zkaric1 <zkaric1@etf.unsa.ba>
2019-05-14 18:52:59 +0200

    prepravljeni testovi, jer su neki podaci izbrisani iz baze

--- Routes/Unos/unos_za_test.js
+++ Routes/Unos/unos_za_test.js
@@ -18,6 +18,30 @@ let predmet = {
     opis: 'testni predmet'
 }
 
+let as = 
+    { 
+        "id": 36,
+        "idOdsjek": 1,
+        "ime": "Almir",
+        "prezime": "Karabegović",
+        "datumRodjenja": "1975-06-22",
+        "JMBG": "2206975842818",
+        "email": "almir.karabegovic@etf.unsa.ba",
+        "mjestoRodjenja": "Zenica",
+        "kanton": "Zeničko-Dobojski kanton",
+        "drzavljanstvo": "BiH",
+        "telefon": "+38761525926",
+        "spol": false,
+        "imePrezimeMajke": "Fatima Marić",
+        "imePrezimeOca": "Nesib Karabegović",
+        "adresa": "Zmaja od Bosne 29",
+        "username": "almir.karabegovic1",
+        "linkedin": "https://ba.linkedin.com/in/almir",
+        "website": null,
+        "titula": null
+   
+}
+
 //ako vec postoji u bazi, test ce past
 test.get('/unesiPredmet', function(req,res) {
 db.Predmet.create(predmet).then( () => {
@@ -27,6 +51,15 @@ db.Predmet.create(predmet).then( () => {
     res.status(400).send('Greska vec postoji predmet sa tim IDem')
 })
 })
+
+test.get('/unesi', function(req,res) {
+    db.Korisnik.create(as).then( () => {
+        res.status(200).send('Uspjesno dodan')
+    }).catch( err => {
+        console.log(err);
+        res.status(400).send('Greska vec postoji predmet sa tim IDem')
+    })
+    })
 
 
 module.exports = test;
