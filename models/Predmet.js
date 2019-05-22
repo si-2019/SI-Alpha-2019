00c08103a8f87dfa88df7ec064f15cf0dafb80b1
ATalic2 <you@example.com>
2019-05-22 23:33:57 +0200

    383

--- models/Predmet.js
+++ models/Predmet.js
@@ -1,42 +1,49 @@
-const Sequelize = require("sequelize");
+/* jshint indent: 2 */
 
-module.exports = function(sequelize,DataTypes){
-    return sequelize.define("Predmet",{
-		id: {
-			type: DataTypes.INTEGER(10),
-			allowNull: false,
-			autoIncrement: true,
-			primaryKey: true
-		},
-        idAsistent:{
-			type: DataTypes.INTEGER(10),
-			allowNull: true
-		},
-		idProfesor:{
-			type: DataTypes.INTEGER(10),
-			allowNull: true
-		},
-		naziv:{
-			type: Sequelize.STRING(255),
-			allowNull: true
-		},
-		ects:{
-			type: DataTypes.INTEGER(10),
-			allowNull: true
-		},
-		brojPredavanja:{
-			type: DataTypes.INTEGER(10),
-			allowNull: true
-		},
-		brojVjezbi:{
-			type: DataTypes.INTEGER(10),
-			allowNull: true
-		},
-		opis:{
-			type: Sequelize.STRING(1024),
-			allowNull: true
-		}
-    },{
-		tableName: 'Predmet'
-	})
+module.exports = function(sequelize, DataTypes) {
+  return sequelize.define('Predmet', {
+    id: {
+      type: DataTypes.INTEGER(10),
+      allowNull: false,
+      primaryKey: true
+    },
+    idAsistent: {
+      type: DataTypes.INTEGER(10),
+      allowNull: true,
+      references: {
+        model: 'Korisnik',
+        key: 'id'
+      }
+    },
+    idProfesor: {
+      type: DataTypes.INTEGER(10),
+      allowNull: true,
+      references: {
+        model: 'Korisnik',
+        key: 'id'
+      }
+    },
+    naziv: {
+      type: DataTypes.STRING(255),
+      allowNull: false
+    },
+    ects: {
+      type: DataTypes.INTEGER(10),
+      allowNull: false
+    },
+    brojPredavanja: {
+      type: DataTypes.INTEGER(10),
+      allowNull: false
+    },
+    brojVjezbi: {
+      type: DataTypes.INTEGER(10),
+      allowNull: false
+    },
+    opis: {
+      type: DataTypes.STRING(1024),
+      allowNull: false
+    }
+  }, {
+    tableName: 'Predmet'
+  });
 };
