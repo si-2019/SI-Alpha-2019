15cdb72df7622ab7755f442463224b82ea25aa0d
zkaric1 <zkaric1@etf.unsa.ba>
2019-05-30 19:47:35 +0200

    promijenjene info za update profesora

--- Routes/Korisnik/korisnikController.js
+++ Routes/Korisnik/korisnikController.js
@@ -120,55 +120,29 @@ await db.Korisnik.findOne({where:{username: req.body.username, idUloga: 3}}).the
         return res.status(400).send({message: 'Profesor se ne nalazi u bazi'});
     }
     else {
-         //validacija
-         var duzine = await validacijaStringova(req.body);
-         console.log(duzine);
+         //validacija       
+        req.body.spol = 1;
+        req.body.mjestoRodjenja = ' ';
+        req.body.JMBG = '0603997175555';
          var provjera = await validacijaPodataka(req.body);
-         
-        await db.Korisnik.findOne({where:{JMBG:req.body.JMBG}}).then( prof => {
-            if(prof != null && prof.username != req.body.username) {
-            console.log('profa'+prof);
-            return res.status(400).send({message: 'Postoji korisnik sa istim JMBG!'});
-            }   
-        }).catch(err => {
-        console.error(err);
-        })
-         var dr = await validacijaDatumaRodjenja(req.body.datumRodjenja);         
-         var dj = await provjeriDatumJmbg(req.body.datumRodjenja, req.body.JMBG);    
-         
-        if(duzine != 'Ok') return res.status(400).send({message: duzine});
-        else if(provjera != 'Ok') return res.status(400).send({message : provjera});
-        else if(!dr) return res.status(400).send({message: 'Neispravan datum rodjenja!'});
-        else if(!dj) return res.status(400).send({message: 'JMBG i datum rodjenja se ne poklapaju'});
-        else {
-         //json kao u pretraga liste
-        var spol = 0;
-        if(podaci.spol == 'zensko') spol = 1;
        
+         if(provjera != 'Ok') return res.status(400).send({message : provjera});     
+        else {       
 
         await db.Odsjek.findOne({where: {naziv: req.body.odsjek}}).then( function(odsjek) {
              var json = {
                  idOdsjek: odsjek.idOdsjek, 
                  ime: podaci.ime, 
-                 prezime: podaci.prezime, 
-                 datumRodjenja: podaci.datumRodjenja, 
-                 JMBG: podaci.JMBG, 
-                 email: podaci.email,
-                 mjestoRodjenja: podaci.mjestoRodjenja,
+                 prezime: podaci.prezime,               
+                 email: podaci.email,              
                  kanton: podaci.kanton,
                  drzavljanstvo: podaci.drzavljanstvo,
-                 telefon: podaci.telefon,
-                 spol: parseInt(spol),  //musko zensko
-                 imePrezimeMajke: podaci.imePrezimeMajke,
-                 imePrezimeOca: podaci.imePrezimeOca,
-                 adresa: podaci.adresa,
-                 username: podaci.username,
-                 //password:null
+                 telefon: podaci.telefon,             
+                 adresa: podaci.adresa,              
                  linkedin: podaci.linkedin,
                  website: podaci.website,
                  titula: podaci.titula};
                 return prof.update(json).then(()=> {
-                    console.log('de pls');
                     return res.status(200).send({message: 'Uspjesno azurirane informacije o profesoru'})
                  });
                 
