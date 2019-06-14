bb286c3e209a8322efcbbe0245243706205997ce
ATalic2 <azurtalic@yahoo.com>
2019-06-14 23:06:46 +0200

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use('/*', (req, res, next) => {
-    res.header('Access-Control-Allow-Origin', process.env.FRONTEND);
+    res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
     res.header('Access-Control-Allow-Headers', 'Content-Type');
     next();
