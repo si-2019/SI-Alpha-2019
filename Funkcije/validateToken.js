3b304e6498b8fcf9174e0caac992ab102a849a0b
ATalic2 <azurtalic@yahoo.com>
2019-06-15 15:18:48 +0200

    Update validateToken.js

--- Funkcije/validateToken.js
+++ Funkcije/validateToken.js
@@ -1,6 +1,6 @@
 require('autorizacija.js')();
 
-module.exports = function(){
+/*module.exports = function(){
 	autentifikacijaAdmin = function(username, token) {
 		$.ajax({
 			url: 'https://si2019romeo.herokuapp.com/users/validate',
@@ -18,5 +18,29 @@ module.exports = function(){
 			}
 		});
 	}
-};
+};*/
 	
+module.exports = function(){
+	autentifikacijaAdmin = function(username, token) {
+		let promise = new Promise((res, rej) => {
+            var ajax = new XMLHttpRequest();
+            ajax.onreadystatechange = function() {
+                if(ajax.readyState == 4 && ajax.status == 200) {
+                    console.log(ajax.status);
+                    if(ajax.status == 200) {
+                        return autorizacijaAdmin(username);
+                    } 
+                    else res(false);
+                }
+                else if(ajax.readyState == 4) {
+                    res(false);
+                }
+            }
+            ajax.open('GET', 'https://si2019romeo.herokuapp.com/users/validate?username=' + username, true);
+            ajax.setRequestHeader("Authorization", token);
+            ajax.send(); 
+        })
+        let result = await promise;
+        return result;        
+	}
+};
