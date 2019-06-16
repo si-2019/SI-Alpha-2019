ac57e7de084aa8ff3d5c09212157effca259d754
ATalic2 <azurtalic@yahoo.com>
2019-06-16 19:14:11 +0200

    Update db.js

--- models/db.js
+++ models/db.js
@@ -1,5 +1,5 @@
 const Sequelize = require("sequelize");
-const sequelize = new Sequelize('KOVRhEJump', 'KOVRhEJump', 'miRyMtJS2r', {
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
     host: process.env.DB_HOST_IP,
     dialect: "mysql",
     logging: false,
