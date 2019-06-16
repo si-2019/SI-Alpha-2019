8fb1edd18349b0d371fbd0c07d71ad60b5dc693c
Emir Sehovic <esehovic2@etf.unsa.ba>
2019-06-16 20:51:56 +0200

    cors popravke

--- index.js
+++ index.js
@@ -12,9 +12,9 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use('/*', (req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'https://si2019frontend.herokuapp.com');
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE');
+  res.header('Access-Control-Allow-Headers', '*');
     next();
 });
 
