564e51315588cefb6f48606403fc1c49a279dd9c
ATalic2 <azurtalic@yahoo.com>
2019-06-16 16:47:29 +0200

    Update Lista odsjeka.js

--- Routes/Odsjek/Lista odsjeka.js
+++ Routes/Odsjek/Lista odsjeka.js
@@ -4,21 +4,11 @@ const odsjekRouter = express.Router();
 const db = require('../../models/db.js');
 db.sequelize.sync();
 
-require('../../Funkcije/validacija.js')();
-require('../../Funkcije/autorizacija.js')();
-require('../../Funkcije/validateToken.js')();
 
 odsjekRouter.get('/GetOdsjeci', async function(req, res){
 	res.contentType('application/json');
-	var currentUser = req.query.currentUser;
-
-    if(!currentUser) return res.status(400).end(JSON.stringify({message: "Nije poslan username korisnika"}));
-
-    var auth = await autorizacijaAdmin(currentUser);
-    console.log(auth);
-    if(!auth) return res.send("Nemate privilegije");
 	console.log("Pretražuje bazu podataka");
-	await db.Odsjek.findAll({
+	db.Odsjek.findAll({
     })
     .then(data => {
 		//data sadrzi listu odsjeka, pretvara se u JSON i šalje
