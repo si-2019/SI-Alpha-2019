2e3d116af85566c6f62ca920423e74e61a74cb06
ATalic2 <azurtalic@yahoo.com>
2019-06-18 12:34:11 +0200

    Update db.js

--- models/db.js
+++ models/db.js
@@ -1,6 +1,6 @@
 const Sequelize = require("sequelize");
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-    host: process.env.DB_HOST_IP,
+    host: "mysql-3213-0.cloudclusters.net",
     dialect: "mysql",
     logging: false,
     port: 3306,
